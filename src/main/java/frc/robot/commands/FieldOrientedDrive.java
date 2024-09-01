93ac4b869be1f177d95445966ffcc86fb6a2f3a4
spamtactics <108055980+spamtactics@users.noreply.github.com>
2024-09-01 15:40:08 +0100

    Update FieldOrientedDrive.java
    Updated it so that it turns off all motors when the command ends

--- src/main/java/frc/robot/commands/FieldOrientedDrive.java
+++ src/main/java/frc/robot/commands/FieldOrientedDrive.java
@@ -87,7 +87,9 @@ public class FieldOrientedDrive extends Command {
 
     // Called once the command ends or is interrupted.
     @Override
-    public void end(boolean interrupted) {}
+    public void end(boolean interrupted) {
+        driveSubsystem.drive(0, 0, 0, false, true);
+    }
 
     // Returns true when the command should end.
     @Override
