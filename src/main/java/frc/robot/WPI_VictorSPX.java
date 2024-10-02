72eaa017a5607d290cdee87bcd0a2bb685f166fb
aDeznyxxyx <Aprilyichenliu@outlook.com>
2024-10-02 16:33:03 +0100

    motor

---
+++ src/main/java/frc/robot/WPI_VictorSPX.java
@@ -0,0 +1,37 @@
+package frc.robot;
+
+
+import edu.wpi.first.util.sendable.Sendable;
+import edu.wpi.first.util.sendable.SendableBuilder;
+import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
+
+public class WPI_VictorSPX{
+    int devicenum;
+    
+
+    public WPI_VictorSPX(int devicenum){
+        this.devicenum=devicenum;
+    }
+
+    public void MotorController(){
+
+    }
+    public void Sendable(){
+
+    }
+    public void AutoCloseable(){
+       
+    }
+    public void	close(){
+
+    }
+    public void	disable(){
+
+    }
+
+    public void	feed(){
+
+    }
+
+
+}
